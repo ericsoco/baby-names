3d67345e26049c7488b881146e6ab47aa32f530a
Eric Socolofsky <eric@transmote.com>
2016-05-16 22:28:37 -0700

    Compute float numeric value for names Other graphic refinements

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -28,10 +28,18 @@ const scatterplot = () => {
 	const onDataLoaded = (error, data) => {
 
 		// slice to subset for testing
-		data = data.slice(0, 100000);
+		// data = data.slice(0, 100000);
 
 		// filter down to only top names per year
-		data = data.filter(d => +d.rank <= 100);
+		data = data.filter(d => +d.rank <= 500);
+
+		// calculate float (0<>26) for each name for alpha sort
+		data.forEach(d => {
+			d.nameNumeric = d.name.split('').reduce((acc, c, i) => {
+				acc += (c.toUpperCase().charCodeAt(0) - 65) / Math.pow(26, i)
+				return acc;
+			}, 0);
+		});
 
 		let yearExtents = d3.extent(data, d => +d.year),
 			fractionExtents = [0, d3.max(data, d => +d.fraction)],
@@ -67,7 +75,7 @@ const scatterplot = () => {
 
 		let rScale = d3.scaleLinear()
 			.domain(domains.fraction)
-			.range([0, 32]);
+			.range([0, 16]);
 
 		let graphContainer = d3.select('#app').append('svg')
 			.attr('width', width + margin.left + margin.right)
@@ -116,9 +124,10 @@ const scatterplot = () => {
 
 		let circlesEnter = circles.enter()
 			.append('circle')
+			.attr('class', d => d.sex)
 			.attr('cx', d => xScale(+d.year))
-			// .attr('cy', d => yScale(d.nameNumeric))	// TODO: fractional values by including more than just first letter
-			.attr('cy', d => yScale(d.name[0].toUpperCase().charCodeAt() - 65))
+			.attr('cy', d => yScale(d.nameNumeric))
+			// .attr('cy', d => yScale(d.name[0].toUpperCase().charCodeAt() - 65))
 			.attr('r', d => rScale(+d.fraction));
 
 	};
