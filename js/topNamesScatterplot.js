c46035dc4e1aae7b517141ea07ef49110b7cf580
Eric Socolofsky <eric@transmote.com>
2016-07-10 21:54:53 -0700

    Cacluate popularity of all names (1000 of each sex per year) over all 135 years. Not using it in the viz yet.

--- js/topNamesScatterplot.js
+++ js/topNamesScatterplot.js
@@ -189,7 +189,10 @@ const topNamesScatterplot = () => {
 						.entries(values),
 					numTopOccurrences: values.reduce((total, d) => {
 						return total + (+d.rank < rankCutoff ? 1 : 0);
-					}, 0)
+					}, 0)/*,
+					popularity: values.reduce((total, d) => {
+						return total + +d.rank
+					}, 0)*/
 				};
 			})
 			.entries(names);
@@ -197,6 +200,58 @@ const topNamesScatterplot = () => {
 		// some names have more numTopOccurrences than there are years in the data,
 		// due to data errors. clamp them to avoid problems.
 		allNames.forEach(name => name.value.numTopOccurrences = Math.min(domains.topOccurrence[1], name.value.numTopOccurrences));
+
+		let namePopularityByYear = d3.nest()
+			.key(d => d.year)
+				.sortKeys(d3.ascending)
+			.rollup(names => names.map(d => ({
+				name: d.name,
+				sex: d.sex,
+				popularity: 1 - d.rank / (names.length / 2)	// each year has a list of equal length for fe/male
+			})))
+			.entries(names);
+		/*
+		^^ generates:
+		[
+			{
+				key: '1880',
+				value: [
+					{ name: 'john', popularity: 0.999 }, ...
+					{ name: 'mary', popularity: 0.999 }, ...
+				]
+			}, ...
+		]
+		*/
+
+		let namePopularity = namePopularityByYear.reduce((acc, yearList) => {
+			let numNames = yearList.value.length / 2;
+			yearList.value.forEach(d => {
+				if (!acc[d.name]) {
+					acc[d.name] = {
+						// each name can exist for both sexes
+						f: [],
+						m: []
+					};
+				}
+				acc[d.name][d.sex].push(d.popularity);
+			});
+			return acc;
+		}, {});
+		/*
+		^^ generates:
+		{
+			'aaron': {
+				'm': [0.8, 0.7, 0.6...],
+				'f': [0.0, 0.05, 0.0...]
+			}
+		}
+		*/
+
+		// final popularity is the sum of the name's popularity across all years the name exists in the data.
+		// therefore, names that appear more often have higher popularity, and rank per year also factors in.
+		allNames.forEach(d => d.value.popularity = namePopularity[d.value.name][d.value.sex].reduce((total, pop) => total + pop));
+
+		console.log(allNames.sort((a, b) => a.value.popularity - b.value.popularity).map(d => `${ d.value.name }: ${ d.value.popularity }`));
 
 		// filter down to only the names that have appeared
 		// in the top { rankCutoff } at least once
